23ca89420267bfab87895573170c7c46fec7db15
Juan Pablo Rodriguez <pablojuan18sc35@gmail.com>
2024-06-10 19:54:57 -0400

    prueba consumo endpoint login

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:school_app/config/constants/environment.dart';
@@ -9,8 +10,42 @@ void main() async {
   runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+    performLogin();
+  }
+
+  void performLogin() async {
+    var dio = Dio();
+    var url =
+        'http://192.168.100.111:8069/api/auth/login'; // Cambia 192.168.x.x con tu dirección IP local real
+    var body = {"username": "pablojuan123@gmail.com", "password": "admin"};
+
+    try {
+      Response response = await dio.post(url, data: body);
+      print('Response status: ${response.statusCode}');
+      print('Response data: ${response.data}');
+    } on DioError catch (e) {
+      if (e.response != null) {
+        print('Dio error:');
+        print('STATUS: ${e.response!.statusCode}');
+        print('DATA: ${e.response!.data}');
+        print('HEADERS: ${e.response!.headers}');
+      } else {
+        print('Error sending request!');
+        print(e.message);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
